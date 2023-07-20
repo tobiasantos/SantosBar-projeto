9368f959971b85c469c94b25081963f9bd0087b3
Tobias Santos <tobiassantosn@gmail.com>
2023-07-20 00:02:41 -0300

    chama

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Router } from "./routes/router";
+import { Toaster } from "react-hot-toast";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
+      <Toaster position="top-center" />
       <Router />
     </>
   );
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "../pages/home";
-import {MenuPage} from "../pages/orderMenu";
+import { MenuPage } from "../pages/orderMenu";
+import { CheckIn } from "../pages/checkIn";
 
 export const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<CheckIn />} />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/menu" element={<MenuPage/>} />
-      
+      <Route path="/menu" element={<MenuPage />} />
     </Routes>
   );
 };
