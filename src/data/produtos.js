5353a585997d165f67a0712608c0015c6c00af68
Tobias Santos <tobiassantosn@gmail.com>
2023-07-20 00:02:34 -0300

    criando produtos

---
+++ src/data/produtos.js
@@ -0,0 +1,45 @@
+import bud from "../assets/produtos/cervejas/bud600.jpg";
+import brahma from "../assets/produtos/cervejas/brahma600.webp";
+import skol from "../assets/produtos/cervejas/skol600.webp";
+import heineken from "../assets/produtos/cervejas/heineken600.webp";
+import stella from "../assets/produtos/cervejas/stella600.jpg";
+import original from "../assets/produtos/cervejas/original600.webp";
+
+export const cervejas = [
+  {
+    id: 1,
+    nome: "Budweiser 600 ml",
+    preco: 13.99,
+    imagem: bud,
+  },
+  {
+    id: 2,
+    nome: "Brahma Duplo Malte 600 ml",
+    preco: 10.99,
+    imagem: brahma,
+  },
+  {
+    id: 3,
+    nome: "Skol 600 ml",
+    preco: 8.99,
+    imagem: skol,
+  },
+  {
+    id: 4,
+    nome: "Heineken 600 ml",
+    preco: 13.99,
+    imagem: heineken,
+  },
+  {
+    id: 5,
+    nome: "Stella Artois 600 ml",
+    preco: 13.99,
+    imagem: stella,
+  },
+  {
+    id: 6,
+    nome: "Original 600 ml",
+    preco: 8.99,
+    imagem: original,
+  },
+];
