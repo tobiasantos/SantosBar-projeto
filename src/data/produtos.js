f0b213e34587de5f2451cb7236d96fc66beff734
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2023-07-20 18:30:19 -0300

    cadastrando petiscos

--- src/data/produtos.js
+++ src/data/produtos.js
@@ -1,51 +1,102 @@
-import bud from "../assets/produtos/cervejas/bud600.jpg";
-import brahma from "../assets/produtos/cervejas/brahma600.webp";
-import skol from "../assets/produtos/cervejas/skoll600.webp";
-import heineken from "../assets/produtos/cervejas/heineken600.webp";
-import stella from "../assets/produtos/cervejas/stella600.jpg";
-import original from "../assets/produtos/cervejas/original600.webp";
+import bud from '../assets/produtos/cervejas/bud600.jpg'
+import brahma from '../assets/produtos/cervejas/brahma600.webp'
+import skol from '../assets/produtos/cervejas/skoll600.webp'
+import heineken from '../assets/produtos/cervejas/heineken600.webp'
+import stella from '../assets/produtos/cervejas/stella600.jpg'
+import original from '../assets/produtos/cervejas/original600.webp'
+import fileCFritas from '../assets/produtos/petiscos/filecfritas.webp'
+import espetinho from '../assets/produtos/petiscos/espetinhos.jpg'
+import frango from '../assets/produtos/petiscos/frango.jpg'
+import macaxeira from '../assets/produtos/petiscos/macaxeira.webp'
+import pastel from '../assets/produtos/petiscos/pastel.jpg'
+import peixe from '../assets/produtos/petiscos/peixe.jpg'
 
 export const cervejas = [
   {
     id: 1,
-    nome: "Budweiser 600 ml",
-    descricao: "Cerveja Budweiser 600 ml",
+    nome: 'Budweiser 600 ml',
+    descricao: 'Cerveja Budweiser 600 ml',
     preco: 13.99,
-    imagem: bud,
+    imagem: bud
   },
   {
     id: 2,
-    nome: "Brahma Duplo Malte 600 ml",
-    descricao: "Cerveja Brahma Duplo Malte 600 ml",
+    nome: 'Brahma Duplo Malte 600 ml',
+    descricao: 'Cerveja Brahma Duplo Malte 600 ml',
     preco: 10.99,
-    imagem: brahma,
+    imagem: brahma
   },
   {
     id: 3,
-    nome: "Skol 600 ml",
-    descricao: "Cerveja Skol 600 ml",
+    nome: 'Skol 600 ml',
+    descricao: 'Cerveja Skol 600 ml',
     preco: 8.99,
-    imagem: skol,
+    imagem: skol
   },
   {
     id: 4,
-    nome: "Heineken 600 ml",
-    descricao: "Cerveja Heineken 600 ml",
+    nome: 'Heineken 600 ml',
+    descricao: 'Cerveja Heineken 600 ml',
     preco: 13.99,
-    imagem: heineken,
+    imagem: heineken
   },
   {
     id: 5,
-    nome: "Stella Artois 600 ml",
-    descricao: "Cerveja Stella Artois 600 ml",
+    nome: 'Stella Artois 600 ml',
+    descricao: 'Cerveja Stella Artois 600 ml',
     preco: 13.99,
-    imagem: stella,
+    imagem: stella
   },
   {
     id: 6,
-    nome: "Original 600 ml",
-    descricao: "Cerveja Original 600 ml",
+    nome: 'Original 600 ml',
+    descricao: 'Cerveja Original 600 ml',
     preco: 8.99,
-    imagem: original,
+    imagem: original
+  }
+]
+
+export const petiscos = [
+  {
+    id: 7,
+    nome: 'Filé com Fritas',
+    descricao: 'Filé ao ponto acompanhado de batatas fritas',
+    preco: 29.99,
+    imagem: fileCFritas
   },
-];
+  {
+    id: 8,
+    nome: 'Frango á Passarinha',
+    descricao: 'Frango frito bem crocante',
+    preco: 23.99,
+    imagem: frango
+  },
+  {
+    id: 9,
+    nome: 'Espetinhos',
+    descricao: 'Espetinhos de Carne ou Frango',
+    preco: 7.5,
+    imagem: espetinho
+  },
+  {
+    id: 10,
+    nome: 'Porção de Minipastéis',
+    descricao: 'Porção com 6 minipastéis (sabores: carne, queijo e frango)',
+    preco: 20.99,
+    imagem: pastel
+  },
+  {
+    id: 11,
+    nome: 'Iscas de Peixe',
+    descricao: 'Iscas de peixe frito',
+    preco: 27.99,
+    imagem: peixe
+  },
+  {
+    id: 12,
+    nome: 'Macaxeira Frita',
+    descricao: 'Porção de macaxeira frita extremamente crocante ',
+    preco: 18.5,
+    imagem: macaxeira
+  }
+]
