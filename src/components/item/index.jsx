f406b60a309303d8cc82426cd2efaf10db856e44
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2023-07-20 17:52:15 -0300

    botao add no componente item

--- src/components/item/index.jsx
+++ src/components/item/index.jsx
@@ -1,7 +1,9 @@
 import * as C from "./styled";
+import { GrAddCircle } from 'react-icons/gr';
 
 export const Item = ({ nome, descricao, preco, img }) => {
   return (
+    
     <>
       <C.ItemRow>
         <div className="card">
@@ -13,7 +15,11 @@ export const Item = ({ nome, descricao, preco, img }) => {
               <p>{nome}</p>
             </div>
             <p className="description">{descricao}</p>
-            <span>{preco}</span>
+            <div className="actions">
+            <span>R$ {preco}</span>
+            <a className="add"> <GrAddCircle/></a>
+            </div>
+            
           </div>
         </div>
       </C.ItemRow>
--- src/components/item/styled.js
+++ src/components/item/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const ItemRow = styled.div`
   .card {
@@ -63,8 +63,17 @@ export const ItemRow = styled.div`
   }
 
   .content span {
-    position: relative;
-    bottom: 12px;
     font-weight: 600;
   }
-`;
+
+  .actions {
+    margin-top: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .actions .add {
+    font-size: 20px;
+  }
+`
